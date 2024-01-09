f8b0809e0737dc3eda6f48836db71af818f05830
Jungbok <sumeing.vogi@gmail.com>
2024-01-09 11:09:09 +0900

    Revert "프로그램"
    This reverts commit c5cfce08544b74478309a9e618d851dd6c367b1b.

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -4,10 +4,7 @@
     {
         static void Main(string[] args)
         {
-            player player = new player();
-            // 위 내용 추가
-            Console.WriteLine("Hello, World!"); // 설명추가
-            // 아래 내용 추가
+            Console.WriteLine("Hello, World!");
         }
     }
 }
