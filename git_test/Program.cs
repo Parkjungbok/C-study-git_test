c5cfce08544b74478309a9e618d851dd6c367b1b
Jungbok <sumeing.vogi@gmail.com>
2024-01-09 11:03:44 +0900

    프로그램

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            player player = new player();
+            // 위 내용 추가
+            Console.WriteLine("Hello, World!"); // 설명추가
+            // 아래 내용 추가
         }
     }
 }
